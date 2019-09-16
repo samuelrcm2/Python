6ee4f858435e63a177251ca2a95bcd875b6f727e
samuelrcm2 <55411951+samuelrcm2@users.noreply.github.com>
2019-09-16 18:01:31 -0300

    40. Remove Assignments to Parameters  and 16. Extract Method
    It was Removed the parameter "num" to every method and positioned in out from the methods. The num parameter was renamed to sortedValue to turn the code cleaner. Another modification was extract the compareGuess from the printGuess

--- Guess_the_number_game.py
+++ Guess_the_number_game.py
@@ -3,35 +3,32 @@
 import simplegui
 import random
 
+global sortedValue;
 
 def new_game():
-    global num
     print("new game")
 
-
 def range_of_100():
-    global num
-    num = random.randrange(0,100)
+    sortedValue = random.randrange(0,100)
     print("your range is 0-100")
 
-
 def range_of_1000():
-    global num
-    num = random.randrange(0,1000)
+    sortedValue = random.randrange(0,1000)
     print("range is 0-1000")
 
-
-def input_guess(guess):
-    global num
-    print("Your Guess is " , guess)
+def compareGuess(guess):
     num1 = int(guess)
-    if num1 == num:
-        print("Correct")
-    elif num1 >= num:
-        print("Greater")
-    elif num1 <= num:
-        print("Lower")
+    if num1 == sortedValue:
+        return "Correct";
+    elif num1 > sortedValue:
+        return "Greater";
+    elif num1 < sortedGues:
+        return "Lower";
 
+def printGuess(guess):
+    print("Your Guess is " , guess)
+    answer = compareGuess(guess)
+    print(answer)
 
 frame = simplegui.create_frame("Guess The Number",200,200)
 frame.add_button("range[0-1000)",range1000)
